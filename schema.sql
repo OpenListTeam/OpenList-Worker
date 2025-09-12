39d00ce87a28664c925007788602443b1cc98e9c
pikachuim <pikachuim@qq.com>
2025-09-12 10:43:01 +0800

    add(main): add database sql

--- schema.sql
+++ schema.sql
@@ -1,64 +1,75 @@
-CREATE TABLE path -- 挂载路径
-(
-    driver_uuid INTEGER AUTOINCREMENT,            -- 路径UUID
-    driver_path TEXT PRIMARY KEY UNIQUE NOT NULL, -- 路径名称
-    driver_type TEXT                    NOT NULL, -- 驱动类型
-    config_data TEXT,                             -- 配置数据
-    server_data TEXT                              -- 服务数据
-);
-
-CREATE TABLE user -- 用户信息
+CREATE TABLE mount -- 挂载路径
 (
     -- 核心信息 ===============================================
-    user_uuid INTEGER AUTOINCREMENT,            -- 用户UUID
-    user_name TEXT PRIMARY KEY UNIQUE NOT NULL, -- 用户名称
-    user_pass TEXT,                             -- 密码SHA2
-    user_mask TEXT,                             -- 用户权限
-    is_enable INTEGER,                          -- 是否启用
+    mount_uuid INTEGER AUTOINCREMENT   NOT NULL, -- 路径UUID
+    mount_path TEXT PRIMARY KEY UNIQUE NOT NULL, -- 路径名称
+    mount_type TEXT                    NOT NULL, -- 驱动类型
+    is_enabled INTEGER                 NOT NULL, -- 是否启用
     -- 拓展信息 ===============================================
-    size_sets INTEGER,                          -- 分片大小
-    size_used INTEGER,                          -- 使用大小
-    auth_data TEXT,                             -- 认证数据
-    conn_data TEXT,                             -- 连接数据
+    drive_conf TEXT,                             -- 配置数据
+    drive_save TEXT                              -- 服务数据
 );
 
-CREATE TABLE grps -- 用户分组
+CREATE TABLE users -- 用户信息
 (
-    grps_name TEXT PRIMARY KEY UNIQUE NOT NULL, -- 分组名称
-    grps_mask TEXT                    NOT NULL, -- 分组掩码
-    is_enable INTEGER                 NOT NULL  -- 是否启用
+    -- 核心信息 ===============================================
+    users_uuid INTEGER AUTOINCREMENT   NOT NULL, -- 用户UUID
+    users_name TEXT PRIMARY KEY UNIQUE NOT NULL, -- 用户名称
+    users_pass TEXT                    NOT NULL, -- 密码SHA2
+    users_mask TEXT                    NOT NULL, -- 用户权限
+    is_enabled INTEGER                 NOT NULL, -- 是否启用
+    -- 拓展信息 ===============================================
+    total_size INTEGER,                          -- 分片大小
+    total_used INTEGER,                          -- 使用大小
+    oauth_data TEXT,                             -- 认证数据
+    mount_data TEXT,                             -- 连接数据
+
 );
 
-CREATE TABLE auth -- 授权认证
+CREATE TABLE group -- 用户分组
 (
-    auth_name TEXT PRIMARY KEY UNIQUE NOT NULL, -- 授权名称
-    is_enable INTEGER                 NOT NULL, -- 是否启用
-    auth_type TEXT                    NOT NULL, -- 授权类型
-    auth_data TEXT                    NOT NULL, -- 授权数据
+    -- 核心信息 ===============================================
+    group_name TEXT PRIMARY KEY UNIQUE NOT NULL, -- 分组名称
+    group_mask TEXT                    NOT NULL, -- 分组掩码
+    is_enabled INTEGER                 NOT NULL  -- 是否启用
 );
-CREATE TABLE encs -- 加密配置
+
+CREATE TABLE oauth -- 授权认证
 (
-    encs_name TEXT PRIMARY KEY UNIQUE NOT NULL, -- 加密名称
-    is_enable INTEGER                 NOT NULL, -- 是否启用
-    main_pass TEXT                    NOT NULL, -- 主要密码
-    encs_type INTEGER                 NOT NULL, -- 加密类型
-    encs_mode INTEGER                 NOT NULL, -- 加密模式
-    encs_self INTEGER                 NOT NULL, -- 自动解密
-    rand_pass INTEGER                 NOT NULL, -- 随机密码
-    auth_data TEXT,                             -- 认证数据
-    is_subfix TEXT,                             -- 启用后缀
-    subs_name TEXT                              -- 后缀名称
+    -- 核心信息 ===============================================
+    oauth_name TEXT PRIMARY KEY UNIQUE NOT NULL, -- 授权名称
+    oauth_type TEXT                    NOT NULL, -- 授权类型
+    oauth_data TEXT                    NOT NULL, -- 授权数据
+    is_enabled INTEGER                 NOT NULL, -- 是否启用
 );
-CREATE TABLE mate -- 元组配置
+
+CREATE TABLE crypt -- 加密配置
 (
-    path_name TEXT PRIMARY KEY UNIQUE NOT NULL, -- 元组路径
-    is_enable INTEGER                 NOT NULL, -- 是否启用
-    is_hidden INTEGER                 NOT NULL, -- 是否隐藏
-    is_shared INTEGER                 NOT NULL, -- 是否共享
-    path_mask INTEGER                 NOT NULL, -- 权限掩码
-    path_user INTEGER                 NOT NULL, -- 所有用户
-    part_size INTEGER,                          -- 分片大小
-    encs_name TEXT,                             -- 加密配置
-    zips_data TEXT,                             -- 压缩配置
-    time_save INTEGER                           -- 缓存时间
+    -- 核心信息 ===============================================
+    crypt_name TEXT PRIMARY KEY UNIQUE NOT NULL, -- 加密名称
+    crypt_pass TEXT                    NOT NULL, -- 主要密码
+    crypt_type INTEGER                 NOT NULL, -- 加密类型
+    crypt_mode INTEGER                 NOT NULL, -- 加密模式
+    is_enabled INTEGER                 NOT NULL, -- 是否启用
+    -- 拓展信息 ===============================================
+    crypt_self INTEGER,                          -- 自动解密
+    rands_pass INTEGER,                          -- 随机密码
+    oauth_data TEXT,                             -- 认证数据
+    write_name TEXT,                             -- 后缀名称
+);
+
+CREATE TABLE mates -- 元组配置
+(
+    -- 核心信息 ===============================================
+    mates_name TEXT PRIMARY KEY UNIQUE NOT NULL, -- 元组路径
+    mates_mask INTEGER                 NOT NULL, -- 权限掩码
+    mates_user INTEGER                 NOT NULL, -- 所有用户
+    is_enabled INTEGER                 NOT NULL, -- 是否启用
+    -- 拓展信息 ===============================================
+    dir_hidden INTEGER,                          -- 是否隐藏
+    dir_shared INTEGER,                          -- 是否共享
+    set_zipped TEXT,                             -- 压缩配置
+    set_parted TEXT,                             -- 分片配置
+    crypt_name TEXT,                             -- 加密配置
+    cache_time INTEGER                           -- 缓存时间
 );
