2170148bb7e4bca3b3f54163691fa6c5ef7ba2f9
Polonium-salts <c@cereals.fun>
2026-07-12 02:54:05 +0800

    jkhmjkj

--- api/[...route].ts
+++ api/[...route].ts
@@ -1,21 +1,10 @@
 import { handle } from 'hono/vercel'
 import app from '../src/backend/index'
 
-const vercelHandler = handle(app)
-
-export default async function handler(req: any, res: any, ctx: any) {
-  // Detect if running in Edge Runtime (Fetch API)
-  if (req instanceof Request || (req.headers && typeof req.headers.get === 'function')) {
-    return app.fetch(req, res, ctx)
-  }
-  // Otherwise, running in Node.js (Vercel Serverless)
-  // @ts-ignore
-  return vercelHandler(req, res)
-}
-
-export const GET = handler
-export const POST = handler
-export const PUT = handler
-export const DELETE = handler
-export const PATCH = handler
-export const OPTIONS = handler
+export default handle(app)
+export const GET = handle(app)
+export const POST = handle(app)
+export const PUT = handle(app)
+export const DELETE = handle(app)
+export const PATCH = handle(app)
+export const OPTIONS = handle(app)
